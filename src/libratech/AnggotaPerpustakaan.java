f1a5a08a415ba4e927145ea0fa518f5d42314cdb
Widyasti Bella Kurnia <112605699+widyastibella@users.noreply.github.com>
2023-11-29 19:54:19 +0700

    Update AnggotaPerpustakaan.java

--- src/libratech/AnggotaPerpustakaan.java
+++ src/libratech/AnggotaPerpustakaan.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package libratech;
 
 import java.util.ArrayList;
@@ -14,29 +10,21 @@ public class AnggotaPerpustakaan {
     private String riwayatPeminjaman;
     private int denda;
     
-    public AnggotaPerpustakaan(String nama, int nomorAnggota, String alamat, String riwayatPeminjaman, int denda) {
-        this.nama = nama;
-        this.nomorAnggota = nomorAnggota;
-        this.alamat = alamat;
-        this.riwayatPeminjaman = riwayatPeminjaman;
-        this.denda = denda;
-    }
-    
-    
     private TransaksiPeminjaman transaksi;
     private Admin admin;
-    public AnggotaPerpustakaan(Admin admin, String nama, int nomorAnggota, String alamat, String riwayatPeminjaman, int denda,TransaksiPeminjaman transaksi) {
+    
+    public AnggotaPerpustakaan(Admin admin, String nama, int nomorAnggota, String alamat, String riwayatPeminjaman, int denda) {
+  
         this.admin = admin;
         this.nama = nama;
         this.nomorAnggota = nomorAnggota;
         this.alamat = alamat;
         this.riwayatPeminjaman = riwayatPeminjaman;
         this.denda = denda;
-        this.transaksi = transaksi;
+        this.transaksi = new TransaksiPeminjaman(); 
         admin.addAnggota(this);
     }
-    
-    
+
     public void melihatBuku(){
         System.out.println("============================= Daftar Buku ===============================\n");
         for(Buku value : admin.daftarbuku){
@@ -74,87 +62,88 @@ public class AnggotaPerpustakaan {
     StringBuilder rekapTransaksi = new StringBuilder();
     ArrayList<Buku> bukuPinjaman = new ArrayList<>();
     
-    public void pinjamBuku(String judul){
+    public void pinjamBuku(String judul) {
         StringBuilder pinjam = new StringBuilder();
         boolean ditemukan = false;
         System.out.println("\n=========================== Peminjaman Buku =============================");
-        for (Buku value : admin.daftarbuku){
-            if (value.getJudul().equals(judul) && value.isStatusKetersediaan()){
-                pinjam.append("Peminjam          : "+ nama).append("\nNomor Anggota     : " + nomorAnggota);
-                pinjam.append("\nMeminjam Buku     : " + value.getJudul());
-                bukuPinjaman.add(value);
-                //admin.daftarbuku.remove(value);
-                value.setStatusKetersediaan(false);
-                ditemukan = true;
-                rekapTransaksi.append(judul + ", ");
-                riwayatPeminjaman = rekapTransaksi.toString();
-                this.totalPinjaman += 1;
-                break;
+
+        if (totalPinjaman < 3) {
+            for (Buku value : admin.daftarbuku) {
+                if (value.getJudul().equals(judul) && value.isStatusKetersediaan()) {
+                    pinjam.append("Peminjam          : " + nama).append("\nNomor Anggota     : " + nomorAnggota);
+                    pinjam.append("\nMeminjam Buku     : " + value.getJudul());
+                    bukuPinjaman.add(value);
+                    value.setStatusKetersediaan(false);
+                    ditemukan = true;
+                    rekapTransaksi.append(judul).append(", ");
+                    riwayatPeminjaman = rekapTransaksi.toString();
+                    this.totalPinjaman += 1;
+                    break;
+                }
             }
+
+            if (ditemukan) {
+                transaksi.setIdTransaksi(transaksi.getIdTransaksi() + 1);
+                pinjam.append("\nId Transaksi      : " + transaksi.getIdTransaksi()).append("\nWaktu Peminjaman  : " + transaksi.getWaktuPeminjaman())
+                    .append(" Durasi Max Peminjaman : " + transaksi.getDurasiPeminjaman() + " Hari\n").append("\nH-3 Jadwal Buku Dikembalikan!\n");
+                riwayatTransaksi = new TransaksiPengembalian(judul, transaksi.getIdTransaksi(), transaksi.getWaktuPeminjaman(), denda);
+                ditemukan = false;
+                daftarTransaksi.add(riwayatTransaksi);
+                Notifikasi aktivitas = new Notifikasi(riwayatTransaksi, transaksi, this, this);
+                admin.aktivitas.add(aktivitas);
+            } else {
+                System.out.println(judul + " Tidak Ditemukan atau Tidak Tersedia\n");
+            }
+        } else {
+            System.out.println("Maaf, Anda telah mencapai batas peminjaman maksimal (3 buku)\n");
         }
-        if(ditemukan){
-            transaksi.setIdTransaksi(transaksi.getIdTransaksi() + 1);
-            pinjam.append("\nId Transaksi      : "+ transaksi.getIdTransaksi()).append("\nWaktu Peminjaman  : " + transaksi.getWaktuPeminjaman())
-              .append(" Durasi Max Peminjaman : " + transaksi.getDurasiPeminjaman() + " Hari\n").append("\nH-3 Jadwal Buku Dikembalikan!\n");
-            riwayatTransaksi = new TransaksiPengembalian(judul, transaksi.getIdTransaksi(), transaksi.getWaktuPeminjaman(), denda);
-            ditemukan = false;
-            daftarTransaksi.add(riwayatTransaksi);
-            Notifikasi aktivitas = new Notifikasi(riwayatTransaksi, transaksi, this, this);
-            admin.aktivitas.add(aktivitas);
-        }else{
-            System.out.print(judul + " Tidak Ditemukan\n");
-        }
-        System.out.println(pinjam.toString());
     }
 
-    
-    public void mengembalikanBuku(String judul, int durasiPengembalian){
+
+    public void mengembalikanBuku(String judul, int durasiPengembalian) {
         boolean berhasil = false;
-        for(Buku value  : bukuPinjaman){
-            if(value.getJudul().equals(judul)){
-//                admin.daftarbuku.add(value);
+        for (Buku value : bukuPinjaman) {
+            if (value.getJudul().equals(judul)) {
                 value.setStatusKetersediaan(true);
                 bukuPinjaman.remove(value);
                 berhasil = true;
                 break;
-            }else{
-                System.out.println("Tidak Ada Buku Yang Sesuai!");
             }
         }
-        if (berhasil){
+        if (berhasil) {
             System.out.println("========================== Pengembalian Buku ============================");
-            System.out.println("Nama             : "+nama + "\nID               : " + nomorAnggota);
+            System.out.println("Nama             : " + nama + "\nID               : " + nomorAnggota);
             System.out.println("Berhasil Mengembalikan Buku " + judul);
             System.out.println("Waktu Peminjaman : " + transaksi.getWaktuPeminjaman());
             System.out.print("Durasi Peminjaman " + durasiPengembalian + " Hari");
-            if(transaksi.getDurasiPeminjaman() < durasiPengembalian){
-                if(durasiPengembalian - transaksi.getDurasiPeminjaman() == 1){
+            if (transaksi.getDurasiPeminjaman() < durasiPengembalian) {
+                if (durasiPengembalian - transaksi.getDurasiPeminjaman() == 1) {
                     this.denda += riwayatTransaksi.getTotalDenda();
                     System.out.println(" Dengan Denda " + riwayatTransaksi.getTotalDenda() + "K\n");
-                }else if(durasiPengembalian - transaksi.getDurasiPeminjaman() == 2){
+                } else if (durasiPengembalian - transaksi.getDurasiPeminjaman() == 2) {
                     this.denda += (riwayatTransaksi.getTotalDenda() * 2);
-                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda()*2) + "K\n");
-                }else if(durasiPengembalian - transaksi.getDurasiPeminjaman() == 3){
+                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda() * 2) + "K\n");
+                } else if (durasiPengembalian - transaksi.getDurasiPeminjaman() == 3) {
                     this.denda += (riwayatTransaksi.getTotalDenda() * 3);
-                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda()*3) + "K\n");
-                }else if(durasiPengembalian - transaksi.getDurasiPeminjaman() == 4){
+                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda() * 3) + "K\n");
+                } else if (durasiPengembalian - transaksi.getDurasiPeminjaman() == 4) {
                     this.denda += (riwayatTransaksi.getTotalDenda() * 4);
-                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda()*4) + "K\n");
-                }else if(durasiPengembalian - transaksi.getDurasiPeminjaman() == 5){
+                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda() * 4) + "K\n");
+                } else if (durasiPengembalian - transaksi.getDurasiPeminjaman() == 5) {
                     this.denda += (riwayatTransaksi.getTotalDenda() * 5);
-                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda()*5) + "K\n");
-                }else{
+                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda() * 5) + "K\n");
+                } else {
                     this.denda += (riwayatTransaksi.getTotalDenda() * 50);
-                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda()*50) + "K\n");
+                    System.out.println(" Dengan Denda " + (riwayatTransaksi.getTotalDenda() * 50) + "K\n");
                 }
-            }else{
+            } else {
                 System.out.println(" Dan Tidak Ada Denda\n");
             }
-            berhasil = false;
+        } else {
+            System.out.println("Tidak Ada Buku Yang Sesuai atau Buku Tidak Dikembalikan!\n");
         }
     }
-    
-    
+
     public void tampilkanTransaksi(){
         System.out.println("=========================== Total Transaksi =============================");
         System.out.println("Total Transaksi " + nama);;
@@ -222,4 +211,13 @@ public class AnggotaPerpustakaan {
     public String toString() {
         return "Nama: " + this.nama + " Nomor Anggota: " + this.nomorAnggota;
     }
+    
+    public boolean checkBookAvailability(String judul, Iterable<Buku> daftarbuku) {
+    for (Buku buku : daftarbuku) {
+        if (buku.getJudul().equalsIgnoreCase(judul) && buku.isStatusKetersediaan()) {
+            return true;
+        }
+    }
+    return false;
+}
 }
