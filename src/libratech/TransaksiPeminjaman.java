7455ef8a7d1faeb0fd571a62340cb991d6743dc0
Widyasti Bella Kurnia <112605699+widyastibella@users.noreply.github.com>
2023-11-29 19:57:17 +0700

    Update TransaksiPeminjaman.java

--- src/libratech/TransaksiPeminjaman.java
+++ src/libratech/TransaksiPeminjaman.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package libratech;
-
-/**
- *
- * @author Fuad.H
- */
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
